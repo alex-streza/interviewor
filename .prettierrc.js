764e5046be2dff05465d39394a83ef7b67f3a2d7
alex-streza <alex.streza@okapistudio.com>
2022-09-20 12:03:33 +0300

    add prettier & husky configs

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  plugins: [],
+  bracketSpacing: true,
+  bracketSameLine: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  semi: false,
+  endOfLine: 'lf',
+  tabWidth: 2,
+  useTabs: false,
+  printWidth: 80,
+  arrowParens: 'always',
+}
