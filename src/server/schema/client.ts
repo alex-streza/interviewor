8ac0e34e9c3c155dc15caf570b7c4f14443b97e8
alex-streza <alex.streza@okapistudio.com>
2022-10-09 16:46:17 +0300

    test build

--- src/server/schema/client.ts
+++ src/server/schema/client.ts
@@ -5,6 +5,7 @@ let prisma: PrismaClient
 
 //check if we are running in production mode
 if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
 } else {
   // check if there is already a connection to the database
   // @ts-ignore
